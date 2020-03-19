1bc55fbdb38713bfb9cbe3715d74b86326b05cae
ca2roaldsoy <roaldsoyen@gmail.com>
2020-03-19 21:41:32 +0100

    added navigation and path to different sites. Changed file name for each page

---
+++ src/components/Menu/NavMenu.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+function NavMenu() {
+  return (
+    <Router>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="./" exact>
+          CRGames
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link to="./" exact>
+            Home
+          </Nav.Link>
+          <Nav.Link to="./contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar>
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/game/:id" component={GameDetail} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default NavMenu;
