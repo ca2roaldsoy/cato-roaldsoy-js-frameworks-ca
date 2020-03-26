e5823242b875f8d893b5ebbfd9da48bbad37465a
ca2roaldsoy <roaldsoyen@gmail.com>
2020-03-26 18:37:05 +0100

    added no result message

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,13 +5,11 @@ import HomeComp from "./HomeComp";
 import CardDeck from "react-bootstrap/CardDeck";
 import Spinner from "react-bootstrap/Spinner";
 import Title from "../../constants/title";
-import Button from "react-bootstrap/Button";
 
-function Home(e) {
+function Home() {
   const [games, setGames] = useState([]);
   const [filterGames, setFilterGames] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [favourite, setFavourite] = useState(true);
 
   // fetching game api
   useEffect(() => {
@@ -26,7 +24,7 @@ function Home(e) {
   }, []);
 
   // filter search input
-  const handleInput = () => {
+  const handleInput = e => {
     const inputValue = e.target.value.toLowerCase();
     const filterArr = games.filter(function(game) {
       if (game.name.toLowerCase().indexOf(inputValue) !== -1) {
@@ -49,28 +47,36 @@ function Home(e) {
     );
   }
 
-  // Home Page
+  function searchResult() {
+    if (filterGames.length === 0) {
+      return (
+        <>
+          <h4>Sorry. No result :( </h4>
+        </>
+      );
+    }
+    // Map over games displayed
+    return filterGames.map(game => {
+      const { id, name, background_image, rating, released } = game;
+
+      return (
+        <HomeComp
+          key={id}
+          id={id}
+          name={name}
+          image={background_image}
+          rating={rating}
+          release={released}
+        />
+      );
+    });
+  }
+
   return (
     <>
       <Title title="Games" />
       <Search handleInput={handleInput} />
-      <CardDeck>
-        {/* Map over games displayed*/}
-        {filterGames.map(games => {
-          const { id, name, background_image, rating, released } = games;
-
-          return (
-            <HomeComp
-              key={id}
-              id={id}
-              name={name}
-              image={background_image}
-              rating={rating}
-              release={released}
-            />
-          );
-        })}
-      </CardDeck>
+      <CardDeck>{searchResult()}</CardDeck>
     </>
   );
 }
