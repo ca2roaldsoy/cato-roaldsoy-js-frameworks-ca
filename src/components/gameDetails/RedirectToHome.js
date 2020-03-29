930530b19c179ba8b858e03873cdbd2b05637883
ca2roaldsoy <roaldsoyen@gmail.com>
2020-03-29 15:41:28 +0200

    added proptypes to redirect page

--- src/components/gameDetails/RedirectToHome.js
+++ src/components/gameDetails/RedirectToHome.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import PropTypes from "prop-types";
 
 function RedirectToHome({ timer, setTimer }) {
   //Redirect back to home page after 4 sec.
@@ -11,6 +12,11 @@ function RedirectToHome({ timer, setTimer }) {
     const counter = timer > 0 && setInterval(() => setTimer(timer - 1), 1000);
     return () => clearInterval(counter);
   }, [timer, setTimer]);
+
+  RedirectToHome.propTypes = {
+    timer: PropTypes.number.isRequired,
+    setTimer: PropTypes.func.isRequired
+  };
 
   return <div>{timer}</div>;
 }
